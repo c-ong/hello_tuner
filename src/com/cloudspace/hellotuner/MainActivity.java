8e4cc7028b99f1a79cdd9d04913751bf158f4ec6
Adam Dunson <adam@cloudspace.com>
2012-08-16 09:47:42 -0400

    Leftover changes from last week

--- src/com/cloudspace/hellotuner/MainActivity.java
+++ src/com/cloudspace/hellotuner/MainActivity.java
@@ -3,30 +3,28 @@ package com.cloudspace.hellotuner;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.support.v4.app.NavUtils;
 
 public class MainActivity extends Activity implements OnClickListener {
 
 	private Listener listener = new Listener();
 	private boolean running = false;
-	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button start = (Button)findViewById(R.id.start);
-        start.setOnClickListener(this);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		Button start = (Button)findViewById(R.id.start);
+		start.setOnClickListener(this);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
 
 	public void onClick(View v) {
 		switch(v.getId())  {
